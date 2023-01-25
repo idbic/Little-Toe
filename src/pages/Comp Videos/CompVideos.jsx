4ca2432aab02cfdc1596a955f2bf826a5c4bdc18
idbic <dbicga@gmail.com>
2023-01-25 13:57:07 -0700

    created a grid for my videos section that will display all videos from json file or from database

--- src/pages/Comp Videos/CompVideos.jsx
+++ src/pages/Comp Videos/CompVideos.jsx
@@ -1,4 +1,7 @@
 import YoutubeEmbed from "../../components/YoutubeEmbed/YoutubeEmbed";
+import compVideos from '../../Json/compVideos.json'
+import {Row, Col} from 'react-simple-flex-grid'
+
 
 export default function CompVid() {
     
@@ -7,6 +10,15 @@ export default function CompVid() {
       
         <>
         <YoutubeEmbed embedId="MLyy-ojHEm8"></YoutubeEmbed>
+
+        <Row gutter={40}>
+  {compVideos.map(co => 
+    <Col 
+      xs={{ span: 6 }} sm={{ span: 4 }} md={{ span: 3 }}
+      lg={{ span: 2 }} xl={{ span: 1 }}
+    >{co.name}<img src={`${co.image}.jpg`} width="100%"/></Col>
+  )}
+</Row>
         </>
     );
   }
